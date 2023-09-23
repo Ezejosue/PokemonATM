b85caaa6c0361d5c7f49597b3cd6f04869ffd683
KevinEnriqueMartinezMartinez <127909032+KevinEnriqueMartinezMartinez@users.noreply.github.com>
2023-09-23 11:24:53 -0600

    SWEET ALERT
    se agrego UN Sweet alert en la funcion de pagos de servicios

--- resources/js/validator.js
+++ resources/js/validator.js
@@ -22,9 +22,26 @@ var constraints = {
 
 // Evento al hacer clic en el botón "Pagar"
 document.getElementById("payButton").addEventListener("click", function () {
+  Swal.fire({
+    title: 'Su pago fue realizado con exito ¡Gracias por usar POKEMONATM!',
+    width: 600,
+    padding: '3em',
+    color: '#080808',
+    background: 'FAF5F5 url(/images/trees.png)',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("resources/img/pikachu2.gif")
+      left top
+      no-repeat
+    `
+  })
+  
+  
   var formValues = {
     servicio: document.getElementById("servicio").value,
     npe: document.getElementById("npe").value,
+
+  
   };
 
   var errors = validate(formValues, constraints);
@@ -41,4 +58,5 @@ document.getElementById("payButton").addEventListener("click", function () {
     // Aquí puedes continuar con el proceso de pago si no hay errores
     console.log("Formulario válido, continuar con el proceso de pago.");
   }
+  
 });

1fd9d1119005d34e5cb0003863941119f4a5aa34
VladimirAlvarado <143980100+VladimirAlvarado@users.noreply.github.com>
2023-09-23 23:29:24 -0600

    agregue los datos eliminados por error

--- resources/js/consulta.js
+++ resources/js/consulta.js
@@ -1,6 +1,6 @@
-function obtenerSaldoDisponible(saldoActual) {
+function obtenerSaldoDisponible(saldoActualPokemon) {
     // Simularemos un saldo de ejemplo (puedes cambiar esto)
-    var saldo = saldoActual ;
+    var saldo = saldoActualPokemon ;
 
     // Mostrar el saldo disponible en el elemento "saldoResultado" del Modal de Consulta de Saldo
     document.getElementById("saldoResultado").textContent = "Saldo disponible: $" + saldo.toFixed(2);
@@ -8,5 +8,5 @@ function obtenerSaldoDisponible(saldoActual) {
 
 // Asignar la función obtenerSaldoDisponible al evento "shown.bs.modal" del Modal de Consulta de Saldo
 $('#pokemonModalConsulta').on('shown.bs.modal', function () {
-    obtenerSaldoDisponible(saldoActual);
+    obtenerSaldoDisponible(saldoActualPokemon);
 });

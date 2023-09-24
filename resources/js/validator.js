60b07596ecca2656bfb11eff43b3632f6f61f65e
Josué Ávalos <aezequiel56@gmail.com>
2023-09-24 13:53:18 -0600

    Update validator.js
    Correción del sweetAler de opción de pago de servicios

--- resources/js/validator.js
+++ resources/js/validator.js
@@ -22,35 +22,9 @@ var constraints = {
 
 // Evento al hacer clic en el botón "Pagar"
 document.getElementById("payButton").addEventListener("click", function () {
-  
-  // Crea una instancia de Audio con la ruta al archivo de sonido de alerta
-const alertSound = new Audio('resources/audio/pokemon.mp3'); // Reemplaza con la ruta correcta
-
-// Crea la alerta de SweetAlert2 con un mensaje de éxito
-Swal.fire({
-  title: 'Su pago fue realizado con éxito ¡Gracias por usar POKEMONATM!',
-  width: 600,
-  padding: '3em',
-  color: '#080808',
-  background: 'FAF5F5 url(/images/trees.png)',
-  backdrop: `
-    rgba(0,0,123,0.4)
-    url("resources/img/pikachu2.gif")
-    left top
-    no-repeat
-  `,
-}).then(() => {
-  // Reproduce el sonido de alerta cuando se muestra la alerta
-  alertSound.play();
-});
-
-  
-  
   var formValues = {
     servicio: document.getElementById("servicio").value,
     npe: document.getElementById("npe").value,
-
-  
   };
 
   var errors = validate(formValues, constraints);
@@ -64,8 +38,29 @@ Swal.fire({
       document.getElementById("errors").innerHTML += errors[key] + "<br>";
     }
   } else {
-    // Aquí puedes continuar con el proceso de pago si no hay errores
+    // Si no hay errores, muestra la alerta de SweetAlert
+
+    // Crea una instancia de Audio con la ruta al archivo de sonido de alerta
+    const alertSound = new Audio("resources/audio/pokemon.mp3"); // Reemplaza con la ruta correcta
+
+    // Crea la alerta de SweetAlert2 con un mensaje de éxito
+    Swal.fire({
+      title: "Su pago fue realizado con éxito ¡Gracias por usar POKEMONATM!",
+      width: 600,
+      padding: "3em",
+      color: "#080808",
+      background: "FAF5F5 url(/images/trees.png)",
+      backdrop: `
+        rgba(0,0,123,0.4)
+        url("resources/img/pikachu2.gif")
+        left top
+        no-repeat
+      `,
+    }).then(() => {
+      // Reproduce el sonido de alerta cuando se muestra la alerta
+      alertSound.play();
+    });
+
     console.log("Formulario válido, continuar con el proceso de pago.");
   }
-  
 });

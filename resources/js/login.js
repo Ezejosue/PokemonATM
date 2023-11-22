b2dff29f4c9c9abb420585281bcb2f2feb4fea77
Josué Ávalos <aezequiel56@gmail.com>
2023-11-22 14:22:39 -0600

    Mis finanzas V1
    Se han eliminado archivos de ChartJS y se ha agregado nueva información al archivo JSON de usuarios. Ahora, al iniciar sesión, los datos correspondientes al usuario se muestran en el modal de 'Mis Finanzas'.

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,109 +1,184 @@
 // Creamos un json con los usuarios que podran acceder al ATM
 var usuarios = [
-        {
-            "Nombre": "Vladimir Alvarado",
-            "PIN": 6666,
-            "Cuenta": "0987654321",
-            "SaldoInicial": 300.00
-        },
-        {
-            "Nombre": "Sofia Lopez",
-            "PIN": 1111,
-            "Cuenta": "1234567890",
-            "SaldoInicial": 500.00
-        },
-        {
-            "Nombre": "Gerardo Marroquin",
-            "PIN": 9999,
-            "Cuenta": "9876543219",
-            "SaldoInicial": 950.00
-        },
-        {
-            "Nombre": "Josue Avalos",
-            "PIN": 7896,
-            "Cuenta": "964785142",
-            "SaldoInicial": 800.00
-        },
-        {
-            "Nombre": "Kevin Martinez",
-            "PIN": 2806,
-            "Cuenta": "0584457894",
-            "SaldoInicial": 700.00
-        }
-    ];
+  {
+    Nombre: "Vladimir Alvarado",
+    PIN: 6666,
+    Cuenta: "0987654321",
+    SaldoInicial: 300.0,
+    transacciones: [
+      { concepto: "Bono", fecha: "2023-01-01", monto: 100.0, tipo: "deposito" },
+      {
+        concepto: "Pago de arancel",
+        fecha: "2023-01-02",
+        monto: 50.0,
+        tipo: "retiro",
+      },
+      {
+        concepto: "Pago de servicio",
+        fecha: "2023-01-03",
+        monto: 75.5,
+        tipo: "deposito",
+      },
+      // más transacciones...
+    ],
+  },
+  {
+    Nombre: "Sofia Lopez",
+    PIN: 1111,
+    Cuenta: "1234567890",
+    SaldoInicial: 500.0,
+    transacciones: [
+      {
+        concepto: "Recibo de luz",
+        fecha: "2023-01-02",
+        monto: 30.0,
+        tipo: "retiro",
+      },
+      {
+        concepto: "Aguinaldo",
+        fecha: "2023-01-04",
+        monto: 200.0,
+        tipo: "deposito",
+      },
+      // más transacciones...
+    ],
+  },
+  {
+    Nombre: "Gerardo Marroquin",
+    PIN: 9999,
+    Cuenta: "9876543219",
+    SaldoInicial: 950.0,
+    transacciones: [
+      {
+        concepto: "Pago de servicio",
+        fecha: "2023-01-01",
+        monto: 150.0,
+        tipo: "deposito",
+      },
+      {
+        concepto: "Compra en SIMAN ",
+        fecha: "2023-01-03",
+        monto: 100.0,
+        tipo: "retiro",
+      },
+      // más transacciones...
+    ],
+  },
+  {
+    Nombre: "Josue Avalos",
+    PIN: 7896,
+    Cuenta: "964785142",
+    SaldoInicial: 800.0,
+    transacciones: [
+      {
+        concepto: "Compra en AEON",
+        fecha: "2023-01-02",
+        monto: 50.0,
+        tipo: "retiro",
+      },
+      {
+        concepto: "Pago de servicio",
+        fecha: "2023-01-04",
+        monto: 100.0,
+        tipo: "deposito",
+      },
+      // más transacciones...
+    ],
+  },
+  {
+    Nombre: "Kevin Martinez",
+    PIN: 2806,
+    Cuenta: "0584457894",
+    SaldoInicial: 700.0,
+    transacciones: [
+      {
+        concepto: "Pago de Servicio",
+        fecha: "2023-01-03",
+        monto: 50.0,
+        tipo: "deposito",
+      },
+      {
+        concepto: "Recibo UBER",
+        fecha: "2023-01-04",
+        monto: 25.0,
+        tipo: "retiro",
+      },
+      // más transacciones...
+    ],
+  },
+];
 // Definir las reglas de validación
 var constraints = {
-    pin: {
-      presence: {
-        message: "Ingrese el PIN es requerido.",
-      },
-      length: {
-        minimum: 4,
-        message: "El PIN debe tener 4 caracteres.",
-      },
+  pin: {
+    presence: {
+      message: "Ingrese el PIN es requerido.",
     },
+    length: {
+      minimum: 4,
+      message: "El PIN debe tener 4 caracteres.",
+    },
+  },
+};
+
+// obtenemos el click del boton iniciar sesion
+document.getElementById("btn-login").addEventListener("click", function () {
+  // aplicamos la reglar de la validación
+  var formValues = {
+    pin: document.getElementById("pin").value,
   };
 
-    // obtenemos el click del boton iniciar sesion
-  document.getElementById("btn-login").addEventListener("click", function () {
-    // aplicamos la reglar de la validación
-    var formValues = {
-      pin: document.getElementById("pin").value,
-    };
-  
-    var errors = validate(formValues, constraints);
-  
-    document.getElementById("errors").innerHTML = "";
-  
-    if (errors) {
-        // recorremos la lista de errores de la validación 
-      for (var key in errors) {
-        document.getElementById("errors").innerHTML += errors[key] + "<br>";
-      }
-    }else{
-        // obtenmos el valor del input del login
-        var pin = document.getElementById("pin").value;
-        /* 
+  var errors = validate(formValues, constraints);
+
+  document.getElementById("errors").innerHTML = "";
+
+  if (errors) {
+    // recorremos la lista de errores de la validación
+    for (var key in errors) {
+      document.getElementById("errors").innerHTML += errors[key] + "<br>";
+    }
+  } else {
+    // obtenmos el valor del input del login
+    var pin = document.getElementById("pin").value;
+    /* 
             Con la funcion filtel buscamos dentro del json un valor en especificio
             En este caso buscamos el valor del input 
         */
-        let user = usuarios.filter((obj) => obj.PIN == pin);
-        if(user.length > 0){
-            // lanzamos la alerta con la animacion de cargando para iniciar session
-            let timerInterval
-            Swal.fire({
-                title: 'Ingresando...',
-                timer: 2000,
-                timerProgressBar: true,
-                didOpen: () => {
-                    Swal.showLoading()
-                    const b = Swal.getHtmlContainer().querySelector('b')
-                    timerInterval = setInterval(() => {
-                        b.textContent = Swal.getTimerLeft()
-                    }, 100)
-                },
-                willClose: () => {
-                    clearInterval(timerInterval)
-                }
-                }).then((result) => {
-                    if (result.dismiss === Swal.DismissReason.timer) {
-                        /* 
+    let user = usuarios.filter((obj) => obj.PIN == pin);
+    if (user.length > 0) {
+      // lanzamos la alerta con la animacion de cargando para iniciar session
+      let timerInterval;
+      Swal.fire({
+        title: "Ingresando...",
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const b = Swal.getHtmlContainer().querySelector("b");
+          timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft();
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        },
+      }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+          /* 
                         Creacion un variable en el localStorage con el objeto del usuarios
                         que ingreso in PIN
                         */
-                        localStorage.setItem("session_user",JSON.stringify(user[0]));
-                        // Redireccionamos al usuario a la pagina inicial
-                        window.location.href = './pagina_inicial.html';
-                    }
-                });
-        }else{
-            Swal.fire({
-                icon: 'error',
-                title: 'PIN Incorrecto'
-            });
-            // limpiamos el input al poner un PIN incorrecto 
-            document.getElementById("pin").value = "";
+          localStorage.setItem("session_user", JSON.stringify(user[0]));
+          // Redireccionamos al usuario a la pagina inicial
+          window.location.href = "./pagina_inicial.html";
         }
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "PIN Incorrecto",
+      });
+      // limpiamos el input al poner un PIN incorrecto
+      document.getElementById("pin").value = "";
     }
-  });
-  
+  }
+});
--- resources/js/validSession.js
+++ resources/js/validSession.js
@@ -2,17 +2,156 @@
     Agreamos un evento de escucha en el DOM para que cuando este cargado por completo
     valide que la session exista
 */
-document.addEventListener("DOMContentLoaded", function() {
-    // obtenemos del localStorage con el metodo getItem
-    var user_data = localStorage.getItem('session_user');
-    // si no tiene valores eso quiere decir que es nulo = null
-    if(user_data == null){
-        location.href = './index.html';
-    }else{
-        // Creamos un variable par asignar los datos de la session en formato json
-        let datos = JSON.parse(user_data);
-        // pasamos a las etiquetas HTML el valor del Nombre y la cuenta.
-        document.getElementById("nombre").innerText= datos.Nombre;  
-        document.getElementById("cuenta").innerText= datos.Cuenta; 
+document.addEventListener("DOMContentLoaded", function () {
+  // obtenemos del localStorage con el metodo getItem
+  var user_data = localStorage.getItem("session_user");
+  // si no tiene valores eso quiere decir que es nulo = null
+  if (user_data == null) {
+    location.href = "./index.html";
+  } else {
+    // Creamos un variable par asignar los datos de la session en formato json
+    let datos = JSON.parse(user_data);
+    // pasamos a las etiquetas HTML el valor del Nombre y la cuenta.
+    document.getElementById("nombre").innerText = datos.Nombre;
+    document.getElementById("cuenta").innerText = datos.Cuenta;
+
+    cargarTransacciones(datos.transacciones);
+    procesarTransaccionesParaGraficos(datos.transacciones);
+    calcularPromedioRetiros(datos.transacciones);
+  }
+});
+
+function cargarTransacciones(transacciones) {
+  if (transacciones) {
+    var tablaTransacciones = document.getElementById("tablaTransacciones");
+    tablaTransacciones.innerHTML = ""; // Limpia la tabla antes de cargar nuevas transacciones
+
+    transacciones.forEach(function (transaccion) {
+      var fila = tablaTransacciones.insertRow();
+      fila.insertCell(0).textContent = transaccion.concepto;
+      fila.insertCell(1).textContent = transaccion.fecha;
+      fila.insertCell(2).textContent = transaccion.tipo;
+      fila.insertCell(3).textContent = `$${transaccion.monto.toFixed(2)}`;
+    });
+  }
+}
+
+function procesarTransaccionesParaGraficos(transacciones) {
+  let totales = { deposito: 0, retiro: 0 };
+
+  // Calcula los totales de depósitos y retiros
+  transacciones.forEach((t) => {
+    if (t.tipo === "deposito") {
+      totales.deposito += t.monto;
+    } else if (t.tipo === "retiro") {
+      totales.retiro += t.monto;
     }
-}); 
+  });
+
+  // Actualiza el gráfico
+  actualizarGraficoDeBarras(totales);
+}
+
+var myChart;
+
+function actualizarGraficoDeBarras(totales) {
+  var ctx = document.getElementById("myChart").getContext("2d");
+
+  // Destruir la instancia actual del gráfico si existe
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  // Crear una nueva instancia del gráfico con los datos actualizados
+  myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Depósitos", "Retiros"],
+      datasets: [
+        {
+          label: "Monto",
+          data: [totales.deposito, totales.retiro],
+          backgroundColor: [
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+          ],
+          borderColor: ["rgba(75, 192, 192, 1)", "rgba(255,99,132,1)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+}
+
+function calcularPromedioRetiros(transacciones) {
+  let totalRetiros = 0;
+  let contadorRetiros = 0;
+
+  // Suma solo las transacciones de tipo 'retiro' y cuenta cuántas hay
+  transacciones.forEach((transaccion) => {
+    if (transaccion.tipo === "retiro") {
+      totalRetiros += transaccion.monto;
+      contadorRetiros++;
+    }
+  });
+
+  // Calcula el promedio de retiros
+  let promedioRetiros =
+    contadorRetiros > 0 ? totalRetiros / contadorRetiros : 0;
+
+  // Actualiza el gráfico
+  actualizarGraficoPromedioRetiros(promedioRetiros);
+}
+
+var myChart2;
+
+function actualizarGraficoPromedioRetiros(promedioRetiros) {
+  var ctx = document.getElementById("myChart2").getContext("2d");
+
+  // Destruir la instancia actual del gráfico si existe
+  if (myChart2) {
+    myChart2.destroy();
+  }
+
+  // Crear una nueva instancia del gráfico con los datos actualizados
+  myChart2 = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: ["Promedio de Retiros"],
+      datasets: [
+        {
+          label: "Promedio de Retiros",
+          data: [promedioRetiros],
+          backgroundColor: ["rgba(255, 159, 64, 0.2)"],
+          borderColor: ["rgba(255, 159, 64, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Promedio de Retiros",
+      },
+      animation: {
+        animateScale: true,
+        animateRotate: true,
+      },
+    },
+  });
+}

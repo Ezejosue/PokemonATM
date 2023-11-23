93c6c6c52988c6dbe28eec6ef63f964e7e802e7f
KevinEnriqueMartinezMartinez <127909032+KevinEnriqueMartinezMartinez@users.noreply.github.com>
2023-11-22 23:56:54 -0600

    se dio funcionalidad a consulta de saldo y retiro
    se agregaron las validaciones correspondientes a consulta de saldo y retiro

--- resources/js/retiro.js
+++ resources/js/retiro.js
@@ -1,94 +1,84 @@
-// Creamos un json con los usuarios que podran acceder al ATM
-var recibos = [
-        {
-            "TipoRecibo": "Agua",
-            "Total": 30.00,
-            "NPE": "1000 3100 9550 6433",
-        },
-        {
-            "TipoRecibo": "CAESS",
-            "Total": 25.00,
-            "NPE": "2346 4507 8943 1347",
-        },
-    ];
 
-
-
-
-
-// Recuperar del localstorage los datos de la session
+// Recuperar del localstorage los datos de la sesión
 let user_data = localStorage.getItem('session_user');
 let datos = JSON.parse(user_data);
-// pasamos del array de  localstorage el saldoInicial
+// pasamos del array de localstorage el saldoInicial
 var saldoActualPokemon = datos.SaldoInicial;
 
 // Definir las reglas de validación
 var constraintsRetiro = {
-    cantidadRetirar: {
-      presence: {
-        message: "Cantidad",
-      },
-      length: {
-        minimum: 1,
-        message: "Requerida",
-      },
+  cantidadRetirar: {
+    presence: {
+      message: "Cantidad requerida",
     },
+    numericality: {
+      greaterThan: 0,
+      message: "Debe ser mayor que cero",
+    },
+  },
+};
+
+document.getElementById("ButtonRetirar").addEventListener("click", function () {
+  var inputRetiro = parseFloat(document.getElementById("cantidadRetirar").value);
+
+  // Verificar si la cantidad a retirar es mayor que el saldo actual
+  if (inputRetiro > saldoActualPokemon) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Saldo insuficiente...',
+      text: 'No puedes retirar más dinero del que tienes en tu cuenta.',
+    });
+    return; // Salir de la función si el saldo es insuficiente
+  }
+
+  var formValuesRetiro = {
+    cantidadRetirar: inputRetiro,
   };
 
-  document.getElementById("ButtonRetirar").addEventListener("click", function () {
-    var formValuesRetiro = {
-        cantidadRetirar: document.getElementById("cantidadRetirar").value,
-    };
-  
-    var errorsRe = validate(formValuesRetiro, constraintsRetiro);
-  
-    document.getElementById("errors_retiro").innerHTML = "";
-  
-    if (errorsRe) {
-      for (var key in errorsRe) {
-        document.getElementById("errors_retiro").innerHTML += errorsRe[key] + "<br>";
-      }
-    }else{
-        var inputRetiro = document.getElementById("cantidadRetirar").value;
-        if(inputRetiro > saldoActualPokemon){
-          Swal.fire({
-              icon: 'error',
-              title: 'Saldo insuficiente... :('
-          });
-        }else{
-          let timerInterval
-          const alertSound = new Audio('resources/audio/pokemon.mp3');
-          Swal.fire({
-          title: 'Retirando Efectivo...',
-          timer: 2000,
-          timerProgressBar: true,
-          didOpen: () => {
-              Swal.showLoading()
-              const b = Swal.getHtmlContainer().querySelector('b')
-              timerInterval = setInterval(() => {
-                  b.textContent = Swal.getTimerLeft()
-              }, 100)
-          },
-          willClose: () => {
-              clearInterval(timerInterval)
-              alertSound.play();
-          }
-          })
-          .then((result) => {
-              /* Read more about handling dismissals below */
-              if (result.dismiss === Swal.DismissReason.timer) {
+  var errorsRe = validate(formValuesRetiro, constraintsRetiro);
 
-                  document.getElementById("cantidadRetirar").value = "";
+  document.getElementById("errors_retiro").innerHTML = "";
 
-                  Swal.fire({
-                      icon: 'success',
-                      title: 'Retire el efectivo'
-                  });
-                  
-                  $("#pokemonModalRetiro").modal('hide');
-              }
-          });
-        }
+  if (errorsRe) {
+    for (var key in errorsRe) {
+      document.getElementById("errors_retiro").innerHTML += errorsRe[key] + "<br>";
     }
-  });
-  
+  } else {
+    let timerInterval;
+    const alertSound = new Audio('resources/audio/pokemon.mp3');
+    Swal.fire({
+      title: 'Retirando Efectivo...',
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        const b = Swal.getHtmlContainer().querySelector('b');
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft();
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+        alertSound.play();
+      }
+    }).then((result) => {
+      if (result.dismiss === Swal.DismissReason.timer) {
+        // Realizar el retiro y actualizar el saldo
+        saldoActualPokemon -= inputRetiro;
+
+        // Actualizar el saldo en el localStorage
+        datos.SaldoInicial = saldoActualPokemon;
+        localStorage.setItem('session_user', JSON.stringify(datos));
+
+        document.getElementById("cantidadRetirar").value = "";
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Retire el efectivo'
+        });
+
+        $("#pokemonModalRetiro").modal('hide');
+      }
+    });
+  }
+});
---
+++ resources/js/servicios.js
@@ -0,0 +1,19 @@
+// Creamos un json con los servicios a pagar
+var recibos = [
+    {
+        "TipoRecibo": "ANDA",
+        "Total": 30.00,
+        "NPE": "1000 3100 9550 6433",
+    },
+    {
+        "TipoRecibo": "CLESA",
+        "Total": 25.00,
+        "NPE": "2346 4507 8943 1347",
+    },
+  {
+    "TipoRecibo": "TIGO",
+        "Total": 45.00,
+        "NPE": "2346 4507 8943 4567",
+  }
+];
+
